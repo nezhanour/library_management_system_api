dead1d988659ae1e23c89914272791a56c0634a4
nezhanour <nezha95nour@gmail.com>
2024-12-29 20:41:40 +0100

    Added MySql settings in settings.py

--- library_management_system_api/settings.py
+++ library_management_system_api/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'library_management_system_api.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'library',
+        'USER': 'root',
+        'PASSWORD': 'NMAbtsZO99@Nc',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
