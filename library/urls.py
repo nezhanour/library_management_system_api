41e98591f44e809d52fadd5124c576af52c747a3
nezhanour <nezha95nour@gmail.com>
2025-01-12 18:11:44 +0100

    registered user router

--- library/urls.py
+++ library/urls.py
@@ -1,11 +1,18 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import BookViewSet, LoanViewSet
+from .views import BookViewSet, UserViewSet, LoanViewSet
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 router = DefaultRouter()
 router.register(r'books', BookViewSet)
+router.register(r'users', UserViewSet)
 router.register(r'loans', LoanViewSet)
 
 urlpatterns = [
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('', include(router.urls)),
 ]
