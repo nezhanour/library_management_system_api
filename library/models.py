5cc3dc7125c85bc4585230746403cbbbfb4ba0a2
nezhanour <nezha95nour@gmail.com>
2025-01-12 17:50:10 +0100

    added user model

--- library/models.py
+++ library/models.py
@@ -1,5 +1,6 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
+
 
 # Book model
 class Book(models.Model):
@@ -7,11 +8,40 @@ class Book(models.Model):
     author = models.CharField(max_length=255)
     isbn = models.CharField(max_length=13, unique=True)  # Ensure unique ISBNs
     published_date = models.DateField()
-    number_of_copies = models.IntegerField()
+    number_of_copies = models.PositiveIntegerField(default=1)
 
     def __str__(self):
         return f'{self.title} by {self.author}'
 
+# User model
+class UserManager(BaseUserManager):
+    def create_user(self, username, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError('The Email field must be set')
+        email = self.normalize_email(email)
+        user = self.model(username=username, email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, username, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        return self.create_user(username, email, password, **extra_fields)
+
+class User(AbstractBaseUser, PermissionsMixin):
+    username = models.CharField(max_length=255, unique=True)
+    email = models.EmailField(unique=True, max_length=255)
+    date_of_membership = models.DateField(auto_now_add=True)
+    is_active = models.BooleanField(default=True)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = ['email']
+
+    def __str__(self):
+        return self.username
 
 # Loan model
 class Loan(models.Model):
@@ -19,6 +49,7 @@ class Loan(models.Model):
     book = models.ForeignKey(Book, on_delete=models.CASCADE)  # Link to Book
     checked_out_date = models.DateTimeField(auto_now_add=True)
     returned_date = models.DateTimeField(null=True, blank=True)  # Allow null for unreturned books
+    is_returned = models.BooleanField(default=False)
 
     def __str__(self):
         return f'{self.user.username} borrowed {self.book.title}'
