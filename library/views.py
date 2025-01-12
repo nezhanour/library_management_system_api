d6a5433be6b3074a0ca3a26dde254341a952dc4c
nezhanour <nezha95nour@gmail.com>
2025-01-12 18:06:15 +0100

    added UserViewSet
    added user view and fixed check_out_book and return_book functions

--- library/views.py
+++ library/views.py
@@ -1,26 +1,50 @@
-from rest_framework import viewsets, status
+from rest_framework import viewsets
 from rest_framework.response import Response
 from rest_framework.decorators import action
-from .models import Book, Loan
-from .serializers import BookSerializer, LoanSerializer
-from django.contrib.auth.models import User
-from django.utils import timezone
+from .models import Book, User, Loan
+from .serializers import BookSerializer, UserSerializer, LoanSerializer
+from rest_framework.permissions import IsAuthenticated
 
 # Book ViewSet
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
 
+    @action(detail=False, methods=['get'], url_path='available')
+    def available_books(self, request):
+        available_books = Book.objects.filter(copies_available__gt=0)
+        serializer = self.get_serializer(available_books, many=True)
+        return Response(serializer.data)
+
+# User ViewSet
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
 # Loan ViewSet
 class LoanViewSet(viewsets.ModelViewSet):
     queryset = Loan.objects.all()
     serializer_class = LoanSerializer
 
-    @action(detail=True, methods=['post'])
+    @action(detail=True, methods=['post'], url_path='check-out')
+    def check_out_book(self, request, pk=None):
+        user = request.user
+        book = self.get_object()
+        if book.copies_available > 0:
+            Transaction.objects.create(user=user, book=book)
+            book.copies_available -= 1
+            book.save()
+            return Response({'message': 'Book checked out successfully.'})
+        return Response({'message': 'No copies available.'}, status=400)
+
+    @action(detail=True, methods=['post'], url_path='return')
     def return_book(self, request, pk=None):
-        loan = self.get_object()
-        loan.returned_date = timezone.now() # Mark the book as returned
-        loan.save()
-        loan.book.number_of_copies += 1 # Increase available copies
-        loan.book.save()
-        return Response({'status': 'book returned'})
+        transaction = self.get_object()
+        if not transaction.is_returned:
+            transaction.is_returned = True
+            transaction.return_date = timezone.now()
+            transaction.save()
+            transaction.book.copies_available += 1
+            transaction.book.save()
+            return Response({'message': 'Book returned successfully.'})
+        return Response({'message': 'Book already returned.'}, status=400)
