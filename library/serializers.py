1c1fa5aa50162d1b4bdea10bc65e281c0543ce89
nezhanour <nezha95nour@gmail.com>
2025-01-12 17:56:40 +0100

    added UserSerializer

--- library/serializers.py
+++ library/serializers.py
@@ -1,10 +1,15 @@
 from rest_framework import serializers
-from .models import Book, Loan
+from .models import Book, User, Loan
 
 class BookSerializer(serializers.ModelSerializer):
     class Meta:
         model = Book
         fields = ['id', 'title', 'author', 'isbn', 'published_date', 'number_of_copies']
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email', 'date_of_membership', 'is_active']
 
 class LoanSerializer(serializers.ModelSerializer):
     class Meta:
